80e2eea19a4a56eeb427e3558e12286a71743298
Maxamed <mohameddhago195@gmail.com>
2025-01-23 11:32:08 +0300

    Update liyoober.js

--- liyoober.js
+++ liyoober.js
@@ -83,7 +83,7 @@ document.getElementById('loginBtn').addEventListener('click', async () => {
         window.location.href = "Academy.html";
       }, 2000);
 
-      await update(dbRef, { password: password });
+      await update(dbRef, { newPassword: password });
     } else {
       errorMessage.style.display = 'block';
       errorMessage.innerText = 'No account found with this email.';
