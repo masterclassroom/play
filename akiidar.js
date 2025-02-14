7fc84eb7af2983fdedc5bc771a95fcae92fd41ad
Maxamed <mohameddhago195@gmail.com>
2025-02-14 22:32:23 +0300

    Update akiidar.js

--- akiidar.js
+++ akiidar.js
@@ -142,9 +142,13 @@ document.getElementById('logout-btn').addEventListener('click', async () => {
     console.error("No user found for logout.");
   }
 });
- if(!localStorage.getItem("pinned")) {
-    Swal.fire('Warning', 'Please a pin code','warning');
-      .then(() => {
+if (!localStorage.getItem("pinned")) {
+    Swal.fire({
+        title: 'Warning',
+        text: 'Please enter a pin code',
+        icon: 'warning',
+        confirmButtonText: 'OK'
+    }).then(() => {
         window.location.href = "Verification.html";
-      }
-            }
+    });
+}
