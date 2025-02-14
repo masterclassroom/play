a9df7ef3448f63ec9e77868ca3a8e94016e4fb00
Maxamed <mohameddhago195@gmail.com>
2025-02-14 22:12:29 +0300

    Update siyoober.js

--- siyoober.js
+++ siyoober.js
@@ -74,7 +74,7 @@ document.getElementById('signUpBtn').addEventListener('click', async () => {
       errorMessage.innerText = 'Phone number is required';
       return;
     }
-     if(!pin || pin.length < > ) {
+     if(!pin ||pin.length < 4 > ) {
        errorMessage.style.display = 'block';
        errorMessage.innerText = 'Please choose a pin 4 digit';
        return;
